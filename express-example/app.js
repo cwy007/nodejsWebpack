47fc16b7b5c8fcd65e37c762625e612124c00ed3
chenweiyan <chanweiyan007@gmail.com>
2022-04-19 08:48:47 +0800

    docs: express router

--- express-example/app.js
+++ express-example/app.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const app = express();
 
+// 路由
+// 字符串
+// 字符串模式
+// 正则表达式
 app.get('/', (req, resp) => {
   resp.json(req.headers);
 });
+
+// 路由参数
+// curl http://localhost:8080/users/1/timelines/2
+// {"userId":"1","timelineId":"2"}
+app.get('/users/:userId/timelines/:timelineId', (req, resp) => (
+  resp.json(req.params)
+));
 
 app.listen(8080, () => {
   console.log('listen on 8080');
